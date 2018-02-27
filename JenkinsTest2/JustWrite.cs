5d489f8adbe455203872f45a6e890d7654f63c5b
Friesenhahn <Joshua.Friesenhahn@Emerson.com>
2018-02-27 09:20:44 -0600

    Add project files.

---
+++ JenkinsTest2/JustWrite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JenkinsTest2
+{
+    public class JustWrite
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(GetMessage());
+        }
+
+        public static string GetMessage()
+        {
+            return "This is a test.";
+        }
+    }
+}
---
+++ JenkinsTestTest/JustWriteTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsTest2;
+
+namespace JenkinsTestTest
+{
+    [TestClass]
+    public class JustWriteTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("This is a test.", JustWrite.GetMessage());
+        }
+    }
+}
